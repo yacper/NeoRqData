e629f9e129fdd56907c06e579dfbe47f2952d2c3
yacper <yacper@gmail.com>
2023-06-07 18:19:17 +0800

    .

--- NeoRqData/Model/RqDataCommon.cs
+++ NeoRqData/Model/RqDataCommon.cs
@@ -306,7 +306,7 @@ public class Tick
 
     //}
 
-    public override string ToString() { return $" {datetime,-30} C:{last,-10} {b1}({b1_v}) -- {a1}({a1_v}) H:{high,-10} L:{low,-10} V:{volume,-10} "; }
+    public override string ToString() { return $"{datetime:yyyy-MM-dd HH:mm:ss.fff}     Last:{last,-10} {b1}({b1_v}) -- {a1}({a1_v})    O:{open,-10} H:{high,-10} L:{low,-10} V:{volume,-10} T:{total_turnover}"; }
 }
 
 public class FutureInfo
